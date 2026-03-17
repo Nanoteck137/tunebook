479b2cc5451248170af82a270421068a00d0318b
Patrik M. Rosenström <patrik.millvik@gmail.com>
2026-03-17 23:50:15 +0100

    Fixed init in dwebble-cli

--- cmd/dwebble-cli/cmd/init.go
+++ cmd/dwebble-cli/cmd/init.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
@@ -12,12 +13,14 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/charmbracelet/huh"
 	"github.com/nanoteck137/dwebble/tools/utils"
 	"github.com/nanoteck137/dwebble/types"
 	"github.com/pelletier/go-toml/v2"
 	"github.com/spf13/cobra"
+	"gopkg.in/vansante/go-ffprobe.v2"
 )
 
 var dateRegex = regexp.MustCompile(`^([12]\d\d\d)`)
@@ -41,6 +44,78 @@ func parseArtist(s string) []string {
 	return artists
 }
 
+func convertMapKeysToLowercase(m map[string]any) map[string]any {
+	res := make(map[string]any)
+	for k, v := range m {
+		res[strings.ToLower(k)] = v
+	}
+
+	return res
+}
+
+type ProbeResult struct {
+	Tags        ffprobe.Tags
+	MediaFormat types.MediaFormat
+	Duration    time.Duration
+}
+
+// TODO(patrik): This is the same as the ProbeMedia from the service.MediaService
+func ProbeMedia(filepath string) (*ProbeResult, error) {
+	ctx := context.TODO()
+
+	probe, err := ffprobe.ProbeURL(ctx, filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	var tags ffprobe.Tags
+	hasGlobalTags := probe.Format.FormatName != "ogg"
+
+	audioStream := probe.FirstAudioStream()
+	if audioStream == nil {
+		// TODO(patrik): Better error?
+		return nil, errors.New("contains no audio streams")
+	}
+
+	if hasGlobalTags {
+		tags = probe.Format.TagList
+	} else {
+		tags = audioStream.TagList
+	}
+
+	tags = convertMapKeysToLowercase(tags)
+
+	dur, err := strconv.ParseFloat(audioStream.Duration, 32)
+	if err != nil {
+		return nil, err
+	}
+
+	duration := time.Duration(dur * float64(time.Second))
+
+	mediaFormat := types.MediaFormatUnknown
+	switch audioStream.CodecName {
+	case "flac":
+		mediaFormat = types.MediaFormatFlac
+	case "pcm_s16le":
+		mediaFormat = types.MediaFormatPcmS16LE
+	case "opus":
+		mediaFormat = types.MediaFormatOpus
+	case "vorbis":
+		mediaFormat = types.MediaFormatVorbis
+	case "mp3":
+		mediaFormat = types.MediaFormatMp3
+	case "aac":
+		mediaFormat = types.MediaFormatAac
+	}
+
+	return &ProbeResult{
+		Tags:        tags,
+		MediaFormat: mediaFormat,
+		Duration:    duration,
+	}, nil
+}
+
+
 type TrackInfo struct {
 	Name   string
 	Artist string
@@ -49,7 +124,7 @@ type TrackInfo struct {
 }
 
 func getTrackInfo(p string) (TrackInfo, error) {
-	probe, err := utils.ProbeTrack(p)
+	probe, err := ProbeMedia(p)
 	if err != nil {
 		return TrackInfo{}, err
 	}
@@ -122,7 +197,7 @@ var initAlbumCmd = &cobra.Command{
 
 		p := tracks[0]
 
-		probe, err := utils.ProbeTrack(p)
+		probe, err := ProbeMedia(p)
 		if err != nil {
 			slog.Error("Failed to probe track", "err", err)
 			os.Exit(-1)
