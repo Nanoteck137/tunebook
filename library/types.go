911c6b5fe52921c365da8a6805a24e7e689ec42b
Patrik M. Rosenström <patrik.millvik@gmail.com>
2026-05-27 15:47:17 +0200

    Moving the library handling to it's own project, tunebook-lib

--- library/types.go
+++ library/types.go
@@ -2,67 +2,8 @@ package library
 
 import "path/filepath"
 
-// TODO(patrik): Move?
-const (
-	libraryMetadataFilename = "library.toml"
-	artistMetadataFilename  = "artist.toml"
-	albumMetadataFilename   = "album.toml"
-)
-
-type LibraryMetadata struct {
-	ExcludedDirs []string `json:"excludedDirs" toml:"excludedDirs"`
-
-	Path string `json:"-" toml:"-"`
-}
-
-type AlbumMetadataGeneral struct {
-	Cover     string   `json:"cover" toml:"cover"`
-	Tags      []string `json:"tags" toml:"tags"`
-	TrackTags []string `json:"trackTags" toml:"trackTags"`
-	Year      int64    `json:"year" toml:"year"`
-}
-
-type AlbumMetadataAlbum struct {
-	Id      string   `json:"id" toml:"id"`
-	Name    string   `json:"name" toml:"name"`
-	Year    int64    `json:"year" toml:"year"`
-	Tags    []string `json:"tags" toml:"tags"`
-	Artists []string `json:"artists" toml:"artists"`
-}
-
-type AlbumMetadataTrack struct {
-	Id      string   `json:"id" toml:"id"`
-	File    string   `json:"file" toml:"file"`
-	Name    string   `json:"name" toml:"name"`
-	Number  int64    `json:"number" toml:"number"`
-	Year    int64    `json:"year" toml:"year"`
-	Tags    []string `json:"tags" toml:"tags"`
-	Artists []string `json:"artists" toml:"artists"`
-}
-
-// TODO(patrik): Rename
-type AlbumMetadata struct {
-	General AlbumMetadataGeneral `json:"general" toml:"general"`
-	Album   AlbumMetadataAlbum   `json:"album" toml:"album"`
-	Tracks  []AlbumMetadataTrack `json:"tracks" toml:"tracks"`
-
-	Path string `json:"-" toml:"-"`
-}
-
-type ArtistMetadata struct {
-	Id string `json:"id" toml:"id"`
-
-	SearchName string   `json:"searchName" toml:"searchName"`
-	Name       string   `json:"name" toml:"name"`
-	Cover      string   `json:"cover" toml:"cover"`
-	Tags       []string `json:"tags" toml:"tags"`
-
-	Path string `json:"-" toml:"-"`
-}
-
 type ArtistEntry struct {
-	Id string `json:"id"`
-
+	Id       string   `json:"id"`
 	Name     string   `json:"name"`
 	CoverArt string   `json:"coverArt"`
 	Tags     []string `json:"tags"`
@@ -78,10 +19,8 @@ func (e ArtistEntry) GetCoverArt() string {
 	return filepath.Join(e.Path, e.CoverArt)
 }
 
-// TODO(patrik): Move to types or library package
 type AlbumEntry struct {
-	Id string `json:"id"`
-
+	Id                 string   `json:"id"`
 	Name               string   `json:"name"`
 	CoverArt           string   `json:"coverArt"`
 	Year               int64    `json:"year"`
@@ -100,10 +39,8 @@ func (e AlbumEntry) GetCoverArt() string {
 	return filepath.Join(e.Path, e.CoverArt)
 }
 
-// TODO(patrik): Move to types or library package
 type TrackEntry struct {
-	Id string `json:"id"`
-
+	Id                 string   `json:"id"`
 	Name               string   `json:"name"`
 	TrackFile          string   `json:"trackFile"`
 	Number             int64    `json:"number"`
