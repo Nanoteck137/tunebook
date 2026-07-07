df9f277c93566e3a8b6666008d68ff4d59d45446
Patrik M. Rosenström <patrik.millvik@gmail.com>
2026-07-08 01:23:47 +0200

    Refactoring/Cleanup config code, fixes #242

---
+++ config/load.go
@@ -0,0 +1,149 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/nanoteck137/tunebook"
+	"github.com/nanoteck137/validate"
+	"github.com/pelletier/go-toml/v2"
+)
+
+func readFileToMap(path string) (map[string]any, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var m map[string]any
+	err = toml.Unmarshal(data, &m)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func mergeMaps(base, overlay map[string]any) {
+	for k, v := range overlay {
+		ov, ok := v.(map[string]any)
+		if !ok {
+			base[k] = v
+			continue
+		}
+
+		bv, ok := base[k]
+		if !ok {
+			base[k] = v
+			continue
+		}
+
+		bvm, ok := bv.(map[string]any)
+		if !ok {
+			base[k] = v
+			continue
+		}
+
+		mergeMaps(bvm, ov)
+	}
+}
+
+func readConfigFromEnv() map[string]any {
+	prefix := strings.ToUpper(tunebook.AppName + "_")
+	m := make(map[string]any)
+
+	for _, e := range os.Environ() {
+		k, v, ok := strings.Cut(e, "=")
+		if !ok {
+			continue
+		}
+
+		if !strings.HasPrefix(k, prefix) {
+			continue
+		}
+
+		key := strings.ToLower(strings.TrimPrefix(k, prefix))
+		m[key] = v
+	}
+
+	return m
+}
+
+func extractConfigOverride(m map[string]any) string {
+	if raw, ok := m["config_override"]; ok {
+		s, _ := raw.(string)
+		return s
+	}
+	return ""
+}
+
+func Load(cfgFile string) (*Config, error) {
+	configMap := configDefaults()
+
+	if cfgFile != "" {
+		m, err := readFileToMap(cfgFile)
+		if err != nil {
+			return nil, fmt.Errorf("reading config: %w", err)
+		}
+
+		mergeMaps(configMap, m)
+	} else {
+		m, err := readFileToMap("config.toml")
+		if err != nil {
+			if !os.IsNotExist(err) {
+				return nil, fmt.Errorf("reading config: %w", err)
+			}
+		} else {
+			mergeMaps(configMap, m)
+		}
+	}
+
+	key := strings.ToUpper(tunebook.AppName + "_CONFIG_OVERRIDE")
+	configOverride := os.Getenv(key)
+	if configOverride == "" {
+		configOverride = extractConfigOverride(configMap)
+	}
+
+	if configOverride != "" {
+		m, err := readFileToMap(configOverride)
+		if err != nil {
+			return nil, fmt.Errorf("reading override config: %w", err)
+		}
+
+		mergeMaps(configMap, m)
+	}
+
+	envMap := readConfigFromEnv()
+	mergeMaps(configMap, envMap)
+
+	var config Config
+
+	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		Metadata:         nil,
+		Result:           &config,
+		WeaklyTypedInput: true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("new decoder: %w", err)
+	}
+
+	err = decoder.Decode(configMap)
+	if err != nil {
+		return nil, fmt.Errorf("decode config: %w", err)
+	}
+
+	oldTag := validate.ErrorTag
+	validate.ErrorTag = "mapstructure"
+	defer func() {
+		validate.ErrorTag = oldTag
+	}()
+
+	err = config.Validate()
+	if err != nil {
+		return nil, fmt.Errorf("invalid config: %w", err)
+	}
+
+	return &config, nil
+}
