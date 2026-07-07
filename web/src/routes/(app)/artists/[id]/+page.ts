272a499dbe6fe7a087cd9e9198763922371cf966
Patrik M. Rosenström <patrik.millvik@gmail.com>
2026-07-07 14:48:09 +0200

    Using Promise.all, fixes #358

--- web/src/routes/(app)/artists/[id]/+page.ts
+++ web/src/routes/(app)/artists/[id]/+page.ts
@@ -4,25 +4,27 @@ import type { PageLoad } from "./$types";
 export const load: PageLoad = async ({ parent, params }) => {
   const data = await parent();
 
-  // TODO(patrik): Use Promise.all
-  const albums = await data.apiClient.getAlbums({
-    query: {
-      filter: `artistId == "${params.id}" || hasFeaturingArtist("${params.id}")`,
-      perPage: "6",
-    },
-  });
+  const [albums, tracks] = await Promise.all([
+    data.apiClient.getAlbums({
+      query: {
+        filter: `artistId == "${params.id}" || hasFeaturingArtist("${params.id}")`,
+        perPage: "6",
+      },
+    }),
+    data.apiClient.getTracks({
+      query: {
+        filter: `artistId == "${params.id}" || hasFeaturingArtist("${params.id}")`,
+        perPage: "5",
+      },
+    }),
+  ]);
+
   if (!albums.success) {
     throw error(albums.error.code, {
       message: albums.error.message,
     });
   }
 
-  const tracks = await data.apiClient.getTracks({
-    query: {
-      filter: `artistId == "${params.id}" || hasFeaturingArtist("${params.id}")`,
-      perPage: "5",
-    },
-  });
   if (!tracks.success) {
     throw error(tracks.error.code, {
       message: tracks.error.message,
